028ae18e36c3f3ee3b2958de533ca4231e3b4435
open-embodied <19910761882@163.com>
2025-06-17 17:12:26 +0800

    mod by YUAN

--- daacoo-ERP-vue/vite.config.js
+++ daacoo-ERP-vue/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
+
+// by DaaCoo www.daacoo.com
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
