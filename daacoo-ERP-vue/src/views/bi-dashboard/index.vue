07077edf8bc2f70ec6f426fe4e5967dae3bed992
open-embodied <19910761882@163.com>
2025-06-18 09:05:41 +0800

    mod by YUAN

---
+++ daacoo-ERP-vue/src/views/bi-dashboard/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import NLIConsole from '@/components/NLIConsole-bi.vue'
+</script>
+
+<template>
+  <div class="dashboard-container">
+    <NLIConsole />
+  </div>
+</template>
+
+<style scoped>
+.dashboard-container {
+  position: absolute;
+  top: -8px;
+  left: -8px;
+  right: -8px;
+  bottom: -8px;
+  overflow: hidden;
+}
+</style>
